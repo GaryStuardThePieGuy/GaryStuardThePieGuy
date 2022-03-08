4d0b3b96477d9ab1f77c3b1d58ff8c4a7a8b652f
kylepmccarthy <kylemcca@upenn.edu>
2022-03-08 18:21:48 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,3 +2,9 @@ $('#contact').click( function(){
     window.open('mailto:garystuard123@gmail.com');
   }
   )
+
+  $('#facebook').click( function(){ 
+    window.open('https://www.facebook.com/search/top?q=the%20pie%20guy');
+  }
+  )
+  
